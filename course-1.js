ad29686a024970bba88b0cbb35ed15a77736264f
wyang14 <weili.yang@yahooinc.com>
2023-02-19 22:13:30 +0000

    claim reference

--- course-1.js
+++ course-1.js
@@ -1,3 +1,7 @@
+/**
+ * This is an example referring to https://github.com/WTFAcademy/WTF-Ethers/tree/main/01_HelloVitalik
+ */
+
 import { ethers } from "ethers";
 const provider = ethers.getDefaultProvider();
 const main = async () => {
