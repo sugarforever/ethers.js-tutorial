9ada88a9da5d37266bd146aba4f414551ee66fc8
wyang14 <weili.yang@yahooinc.com>
2023-02-19 22:12:28 +0000

    feat: course 1 code

---
+++ course-1.js
@@ -0,0 +1,7 @@
+import { ethers } from "ethers";
+const provider = ethers.getDefaultProvider();
+const main = async () => {
+    const balance = await provider.getBalance(`vitalik.eth`);
+    console.log(`ETH Balance of vitalik: ${ethers.utils.formatEther(balance)} ETH`);
+}
+main()
